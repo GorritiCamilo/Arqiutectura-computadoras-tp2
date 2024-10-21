6f75f2f1495a72a747f93c3642d6d55a8d631790
yocamilo <yocamilo99@gmail.com>
2024-10-20 23:02:26 -0300

    Add RX/TX code
    TX and RX code added and baud rate modified

--- project_1.srcs/sources_1/new/UART_BaudRate.v
+++ project_1.srcs/sources_1/new/UART_BaudRate.v
@@ -21,47 +21,41 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module UART_BaudRate
-#(
-    parameter CLOCK_RATE         = 100000000,   // board clock (por defecto 100MHz)
-    parameter BAUD_RATE          = 9600,        // Baud Rate typical
-    parameter RX_OVERSAMPLE_RATE = 16
-)(
-    input wire clk,   // board clock
-    output reg rxClk, // baud rate para rx
-    output reg txClk  // baud rate para tx
-);
+module UART_BaudRate(in_clk,out_tx_en,out_rx_en);
+// El RX CLK esta Oversampled 16 
 
-localparam RX_ACC_MAX   = CLOCK_RATE / (2 * BAUD_RATE * RX_OVERSAMPLE_RATE);
-localparam TX_ACC_MAX   = CLOCK_RATE / (2 * BAUD_RATE);
-localparam RX_ACC_WIDTH = $clog2(RX_ACC_MAX);
-localparam TX_ACC_WIDTH = $clog2(TX_ACC_MAX);
+input wire in_clk;
+output wire out_tx_en;
+output wire out_rx_en;
+/*Numero de ciclos necesarios para habilitar la recepcion/transmicion*/
+parameter RX_MAX = 100_000_000/(115200 * 16);   // 100MHz es in_clk; 115200 es clk_rate
+parameter TX_MAX = 100_000_000/(115200);
 
-reg [RX_ACC_WIDTH-1:0] rx_counter = 0;
-reg [TX_ACC_WIDTH-1:0] tx_counter = 0;
+/*Contadores que van de 0 a RX/TX MAX*/
+reg[15:0] rx_cnt = 0;
+reg[15:0] tx_cnt = 0;
 
-initial begin
-    rxClk = 1'b0;
-    txClk = 1'b0;
-end
+/*Señales de Enable cuando el contador llega a 0*/
+assign out_rx_en = (rx_cnt == 0);
+assign out_tx_en = (tx_cnt == 0);
 
-always @(posedge clk) begin
-    // rx clock
-    if (rx_counter == RX_ACC_MAX[RX_ACC_WIDTH-1:0]) begin
-        rx_counter <= 0;
-        rxClk      <= ~rxClk;
-    end else begin
-        rx_counter <= rx_counter + 1'b1;
+always @(posedge in_clk) begin
+    if (rx_cnt == RX_MAX) begin
+      rx_cnt <= 0;
     end
-
-    // tx clock
-    if (tx_counter == TX_ACC_MAX[TX_ACC_WIDTH-1:0]) begin
-        tx_counter <= 0;
-        txClk      <= ~txClk;
-    end else begin
-        tx_counter <= tx_counter + 1'b1;
+    else begin
+      rx_cnt <= rx_cnt+1;
     end
 end
+
+always @(posedge in_clk) begin
+    if (tx_cnt == TX_MAX) begin
+      tx_cnt <= 0;
+    end
+    else begin
+      tx_cnt <= tx_cnt + 1;
+    end  
+end
+
 
 endmodule
-    
